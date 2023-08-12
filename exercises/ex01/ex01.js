4c809d5778ce83ca50385ba0aec1fe874b97fa98
StefanL71 <140231408+StefanL71@users.noreply.github.com>
2023-08-12 18:48:41 +1200

    Java

---
+++ exercises/ex01/ex01.js
@@ -0,0 +1,5 @@
+alert("Alert#1");
+
+window.addEventListener("load", function() {
+alert("Alert #3");    
+});
---
+++ exercises/ex02/ex02.js
@@ -0,0 +1,15 @@
+window.addEventListener("load", function() {
+    
+ //TODO Your code here.
+ alert("hello");
+let myFirstNumber = 3
+const mySecondNumber = 10.25
+Math.pow(myFirstNumber,2)
+console.log(myFirstNumber);
+console.log(myFirstNumber++);
+console.log(myFirstNumber+7);
+console.log(myFirstNumber-mySecondNumber);
+console.log(mySecondNumber);
+console.log(Math.PI);
+console.log(Math.PI*Math.pow(myFirstNumber,2));
+})
